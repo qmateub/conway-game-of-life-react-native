4e9ad87cd464e8d6af303425991c2bdd795c2cbe
qmateub <qmateub@yahoo.es>
2019-01-22 18:23:25 +0100

    feat(chore): upgrade packages and fix errors

--- components/row/row.js
+++ components/row/row.js
@@ -2,42 +2,36 @@ import React, { Component } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  alive: {
-    width: 10,
-    height: 10,
-    backgroundColor: 'aquamarine',
-    borderColor: 'black',
-    borderWidth: 1,
-    margin: 0.5,
-  },
-  dead: {
-    width: 10,
-    height: 10,
-    backgroundColor: 'white',
-    borderColor: 'black',
-    borderWidth: 1,
-    margin: 0.5,
-  },
+	container: {
+		display: 'flex',
+		flexDirection: 'row',
+	},
+	alive: {
+		width: 10,
+		height: 10,
+		backgroundColor: 'aquamarine',
+		borderColor: 'black',
+		borderWidth: 1,
+		margin: 0.5,
+	},
+	dead: {
+		width: 10,
+		height: 10,
+		backgroundColor: 'white',
+		borderColor: 'black',
+		borderWidth: 1,
+		margin: 0.5,
+	},
 });
 
 export default class Row extends Component {
-  render() {
-    const cells = this.props.row.map((cell, i) => {
-      return <View style={cell === 1 ? styles.alive : styles.dead} key={i}/>;
-    });
+	render() {
+		const cells = this.props.row.map((cell, i) => {
+			return (
+				<View style={cell === 1 ? styles.alive : styles.dead} key={i} />
+			);
+		});
 
-    return (
-      <View style={styles.container}>
-        {cells}
-      </View>
-    );
-  }
+		return <View style={styles.container}>{cells}</View>;
+	}
 }
-
-Row.propTypes = {
-  row: React.PropTypes.array.isRequired,
-};
--- components/start-screen/start-screen.js
+++ components/start-screen/start-screen.js
@@ -1,87 +1,86 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Button, TextInput, Keyboard } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { buildMatrix } from '../../utils';
 import Board from '../board';
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  box: {
-    display: 'flex',
-    flexDirection: 'row',
-    margin: 5,
-  },
-  buttonbox: {
-    margin: 5,
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    flex: 1,
-    padding: 5,
-  },
+	container: {
+		display: 'flex',
+		flex: 1,
+	},
+	box: {
+		display: 'flex',
+		flexDirection: 'row',
+		margin: 5,
+	},
+	buttonbox: {
+		margin: 5,
+	},
+	input: {
+		height: 40,
+		borderColor: 'gray',
+		borderWidth: 1,
+		flex: 1,
+		padding: 5,
+	},
 });
 
 class StartScreenComponent extends Component {
-  state = {
-    width: null,
-    height: null,
-  };
+	state = {
+		width: '',
+		height: '',
+	};
 
-  startGame = () => {
-    Keyboard.dismiss();
-    this.props.navigation.navigate('Board', {
-      width: this.state.width,
-      height: this.state.height,
-      matrix: buildMatrix(this.state.height, this.state.width),
-    });
-  };
+	startGame = () => {
+		Keyboard.dismiss();
+		this.props.navigation.navigate('Board', {
+			width: this.state.width,
+			height: this.state.height,
+			matrix: buildMatrix(this.state.height, this.state.width),
+		});
+	};
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.buttonbox}>
-          <Button
-            disabled={!this.state.height || !this.state.width}
-            onPress={this.startGame}
-            title="Go to board"
-          />
-        </View>
-        <View style={styles.box}>
-          <TextInput
-            style={styles.input}
-            onChangeText={text => this.setState({ height: parseInt(text) })}
-            placeholder={'Rows'}
-            keyboardType="numeric"
-            value={this.state.height}
-          />
-        </View>
-        <View style={styles.box}>
-          <TextInput
-            style={styles.input}
-            onChangeText={text => this.setState({ width: parseInt(text) })}
-            placeholder={'Columns'}
-            keyboardType="numeric"
-            value={this.state.width}
-          />
-        </View>
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.box}>
+					<TextInput
+						style={styles.input}
+						onChangeText={height => this.setState({ height })}
+						placeholder={'Rows'}
+						keyboardType="numeric"
+						value={this.state.height}
+					/>
+				</View>
+				<View style={styles.box}>
+					<TextInput
+						style={styles.input}
+						onChangeText={width => this.setState({ width })}
+						placeholder={'Columns'}
+						keyboardType="numeric"
+						value={this.state.width}
+					/>
+				</View>
+				<View style={styles.buttonbox}>
+					<Button
+						disabled={!this.state.height || !this.state.width}
+						onPress={this.startGame}
+						title="Go to board"
+					/>
+				</View>
+			</View>
+		);
+	}
 }
 
-const StartScreen = StackNavigator({
-  StartScreenComponent: {
-    screen: StartScreenComponent,
-  },
-  Board: {
-    screen: Board,
-  },
+const StartScreen = createStackNavigator({
+	StartScreenComponent: {
+		screen: StartScreenComponent,
+	},
+	Board: {
+		screen: Board,
+	},
 });
 
-export default StartScreen;
+export default createAppContainer(StartScreen);
